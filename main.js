2ab16ab30f9bb873f2dd84417fa85a3c25c5c814
Utissa <tacafela@gmail.com>
2023-09-12 13:07:49 +0700

    update display biar bisa dilihat bagus di hp

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert("Selamat Datang !!! di Little Part Of Utissa");
+window.onscroll = function () {
+  myFunction();
+};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("Navbar");
+  } else {
+    navbar.classList.remove("Navbar");
+  }
+}
